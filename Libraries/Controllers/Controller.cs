0eb77138ae4ec635ff4f46930573e1819d0a459c
Sencer Yazici <senceryazici@Sencers-MacBook-Pro.local>
2017-09-17 19:57:44 +0300

    Adding Controller Manager and disconnecting options

--- Libraries/Controllers/Controller.cs
+++ Libraries/Controllers/Controller.cs
@@ -16,6 +16,57 @@ namespace RoverController
         Joystick
     }
 
+    public class ControllerManager
+    {
+        public static List<Controller> ControllersConnected = new List<Controller>();
+
+        public static List<Controller> ReturnControllersByType(ControllerType Type)
+        {
+            List<Controller> list = new List<Controller>();
+
+            foreach (Controller item in ControllersConnected)
+            {
+                if (item.Type == Type)
+                {
+                    list.add(item);
+                }
+            }           
+            return list; 
+        }
+
+        public static bool[] ReturnControllerStatus()
+        {
+            bool[] status = new bool[ControllersConnected.Count];
+
+            for (int i = 0; i < status.Length; i++)
+            {
+                status[i] = ControllersConnected[i].IsConnected;
+            }
+            return status;
+        }
+
+        public static void DisconnectAll()
+        {
+            foreach (Controller item in ControllersConnected)
+            {
+                item.Disconnect();
+            }
+            return true;
+        }
+
+        public static bool DisconnectByID(string ID)
+        {
+            foreach (Controller item in ControllersConnected)
+            {
+                if (item.ID == ID)
+                {
+                    item.Disconnect();
+                }
+            }
+            return false;
+        }
+    }
+
     public class Controller
     {
         public bool IsConnected { get; set; } = false;
@@ -34,10 +85,15 @@ namespace RoverController
             {
                 IsConnected = ConnectTheController();
             }
+            ControllerManager.ControllersConnected.Add(this);
             return IsConnected;
         }
 
-        
+        public bool Disconnect()
+        {
+            return !IsConnected;
+        }
+
 
         private bool ConnectTheController()
         {
