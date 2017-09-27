81dbfa8dd538f5a180e49b5e7347655ed3d9e5ab
Sencer Yazici <senceryazici@Sencers-MacBook-Pro.local>
2017-09-27 13:05:07 +0300

    Threaded TCP Connections Added!

--- Libraries/Networking/TCPIP.cs
+++ Libraries/Networking/TCPIP.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using System.ComponentModel;
 
 namespace RoverTCPIP
 {
@@ -14,8 +15,39 @@ namespace RoverTCPIP
         public string IPAddress { get; set; }
         public int Port { get; set; }
         public bool ConnectForOneTime { get; set; } = false;
+
+        // When refresh rate is 0, push messages will be sent within a while loop without delay
+        // Be careful about the buffer overflows
         public int RefreshRate { get; set; } = 10;
+
+        // Some server applications exit when the sender closes the stream, to prevent,
+        // set writer cleanup to false
         public bool WriterCleanUpEnabled { get; set; } = false;
+
+        // Push message is the message sending function that works in another thread, 
+        // when it is not "empty", the client will push the messages with the refresh rate (frequency)
+        // This property is private, to access it use StartPushMessage or StopPushMessage Functions.
+        private string PushMessage { get; set; } = "empty";
+
+        // Returns if client is pushing messages to the server
+        public bool IsPushMessageEnabled 
+        {
+            get
+            {
+                if (client.Connected && BG.IsBusy && PushMessage != "empty") return true;
+                else return false;
+            }
+        }
+
+        public bool IsThreaded
+        {
+            get
+            {
+                if (BG.IsBusy && Connected) return true;
+                else return false;
+            }
+        }
+
         public bool Connected
         {
             get
@@ -28,17 +60,56 @@ namespace RoverTCPIP
         {
             this.Port = Port;
             this.IPAddress = IPAddress;
-            
+            BG.WorkerSportsCancellation = true;
+            BG.WorkerReportsProgress = false;
+            BG.DoWork += BackWorkerFunction;
         }
 
         private TcpClient _Client = new TcpClient();
+        private BackgroundWorker BG = new BackgroundWorker();
 
-        public bool SendMessage(string msg)
+        // Threaded
+        public void StartPushMessage(string msg)
         {
-            // Returns True if message is sent 
-            return Send(msg);
+            if(!IsThreaded)
+            {
+                this.DisconnectFromServer();
+                this.BeginConnectionThreaded();
+            }
+            PushMessage = msg;
         }
 
+        public void StopPushMessage()
+        {
+            PushMessage = "empty";
+        }
+
+        public void BackWorkerFunction(object sender, DoWorkEventArgs e)
+        {
+            this.ConnectToServer();
+            while(Connected)
+            {
+                // Keep worker alive
+                if (PushMessage != "empty") Send(PushMessage);
+                if (RefreshRate != 0) System.Threading.Thread.Sleep(1000 / RefreshRate);
+            }
+        }
+
+        public void BeginConnectionThreaded()
+        {
+            if (Connected) this.AbortConnection();
+            BG.RunWorkerAsync();
+        }
+
+        public void AbortConnectionThreaded()
+        {
+            BG.CancelAsync();
+            this.DisconnectFromServer();
+            StopPushMessage();
+        }
+        // Threaded
+
+        // Non-Threaded
         public bool BeginConnection()
         {
             // If Not Connected Begin Connecting Algorithm
@@ -71,8 +142,15 @@ namespace RoverTCPIP
             return DisconnectFromServer();
         }
 
+        public bool SendMessage(string msg)
+        {
+            // Returns True if message is sent 
+            return Send(msg);
+        }
+        // Non-Threaded
+        
 
-
+        // privates
         private bool Send(string msg)
         {
             if (_Client.Connected)
@@ -107,5 +185,6 @@ namespace RoverTCPIP
             _Client.Close();
             return !_Client.Connected;
         }
+        // privates        
     }
 }
